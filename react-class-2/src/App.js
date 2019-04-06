cbf37de15b2a8af08e86f46e447c8b04431bf265
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-04-06 00:26:40 -0300

    Class 2

--- react-class-2/src/App.js
+++ react-class-2/src/App.js
@@ -1,45 +1,64 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
-import Person from "./Person/Person";
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    showPersons: false,
+    showPersons: true,
     persons: [
       {
-        name: "Heitor",
-        age: 31
+        name: 'Heitor',
+        age: 31,
       },
       {
-        name: "Raquel",
-        age: 35
+        name: 'Raquel',
+        age: 35,
       },
       {
-        name: "Marco",
-        age: 3
-      }
-    ]
+        name: 'Marco',
+        age: 3,
+      },
+    ],
   };
 
-togglePersons = () => {
-  this.setState({ showPersons: !this.state.showPersons });
-}
+  togglePersons = () => {
+    this.setState({ showPersons: !this.state.showPersons });
+  };
+
+  switchNameHandler = (newName, index) => {
+    const persons = this.state.persons;
+    persons[index].name = newName;
+    this.setState({ persons });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    console.log('Teste');
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+
+    this.setState({ persons });
+  };
 
   render() {
-
     let persons = null;
 
-    if(this.state.showPersons){
-      persons = (this.state.persons.map((person, index) => <Person key={index} name={person.name} age={person.age} />));
+    if (this.state.showPersons) {
+      persons = this.state.persons.map((person, index) => (
+        <Person
+          key={index}
+          name={person.name}
+          age={person.age}
+          delete={() => this.deletePersonHandler(index)}
+          change={(newName) => this.switchNameHandler(newName, index)}
+        />
+      ));
     }
 
     return (
-      <div className="App">
-        <button
-          onClick={this.togglePersons}
-        >
-          {this.state.showPersons ? "Hide" : "Show"} Persons
+      <div className='App'>
+        <button onClick={this.togglePersons}>
+          {this.state.showPersons ? 'Hide' : 'Show'} Persons
         </button>
         {persons}
       </div>
--- react-class-2/src/Person/Person.js
+++ react-class-2/src/Person/Person.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Person = (props) => {
-    return React.createElement('p', {}, `I'm ${props.name}, age ${props.age}`)
-}
+  return React.createElement(
+    'div',
+    null,
+    React.createElement('p', {}, `I'm ${props.name}, age ${props.age}`),
+    React.createElement('input', {
+      defaultValue: props.name,
+      onChange: (event) => {
+        props.change(event.target.value);
+      },
+    }),
+    React.createElement(
+      'button',
+      {
+        onClick: props.delete,
+      },
+      `[X]`,
+    ),
+  );
+};
 
 export default Person;
