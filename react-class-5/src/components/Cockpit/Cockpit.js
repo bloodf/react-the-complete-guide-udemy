80eb46bab741c773d870d366286e67f08a3abdd3
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-04-08 01:06:04 -0300

    ...

--- react-class-5/src/components/Cockpit/Cockpit.js
+++ react-class-5/src/components/Cockpit/Cockpit.js
@@ -4,10 +4,11 @@ import css from './Cockpit.css';
 const Cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       console.log('Saved Data to Cloud!');
     }, 1000);
     return () => {
+      clearTimeout(timer);
       console.log('[Cockpit.js] cleanup work');
     };
   }, []);
