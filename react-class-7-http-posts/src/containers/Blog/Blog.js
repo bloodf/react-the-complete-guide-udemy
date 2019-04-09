990152004b4eac6016d83809384bdc0d16d73e68
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-04-08 22:18:30 -0300

    Class 8

--- react-class-7-http-posts/src/containers/Blog/Blog.js
+++ react-class-7-http-posts/src/containers/Blog/Blog.js
@@ -5,24 +5,38 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+import axios from 'axios';
+
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    const { data: response } = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts',
+    );
+    this.setState({ posts: response });
+  }
+  render() {
+    return (
+      <div>
+        <section className='Posts'>
+          {this.state.posts.length
+            ? this.state.posts.map((post, index) => (
+                <Post title={post.title} author={post.author} key={post.id} />
+              ))
+            : null}
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
